89ea9c2f6e7cc50cde9e0e19ca9b5b85f4d7d3b8
victor araujo <email.victornascimento@gmail.com>
2025-10-20 21:33:50 -0300

    Desafio Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,24 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
+
         }
 
         public void Ligar()
@@ -18,9 +28,46 @@ namespace DesafioPOO.Models
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação de um nokia...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void LicacaorNokia()
+        {
+            Console.WriteLine("Você está ligando para (81) 984040803.");
+        }
+
+        public void InstalandoApp()
+        {
+            Console.WriteLine("Você clicou para instalar whatsapp");
+        }
+
+        public void AtenderLigacao()
+        {
+
+            Console.WriteLine("digite sim para atender a ligação");
+            string confirmacao = Console.ReadLine();
+            {
+                if (confirmacao.ToLower() == "sim")
+                {
+                    Console.WriteLine("Você atendeu a ligação");
+                }
+
+                else
+                {
+                    Console.WriteLine("Você rejeitou a ligação \n");
+                }
+
+            }
+        }
+
+        public void InstalarInstagram()
+        {
+            Console.WriteLine("Você clicou para instalar o instagram");
+        }
     }
+
+
+
 }
